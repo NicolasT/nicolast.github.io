4a710b2db250f7056dd52538bf730692bb99b677
NicolasT <NicolasT@users.noreply.github.com>
2022-01-14 10:34:21 +0000

    deploy: a0f6d7afaf19c7e229d5371aa8a72552c9521d79

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Hello, world!",
+        "excerpt":"After many years of inactivity (the last entry on eikke.com was written in 2010…) I decided to pick up blogging again. Nothing here yet, expect more content soon! For the curious: the picture above comes from The C Programming Language, First Edition by Kernighan &amp; Ritchie, published in 1978 (archive.org...","categories": ["Various"],
+        "tags": [],
+        "url": "/various/hello-world/",
+        "teaser": null
+      },{
+        "title": "A Git Branching Model for Releases with Generated Files",
+        "excerpt":"Common wisdom dictates not to track generated files under source-control, which is good practice: they clutter diffs and pull-requests, one needs to ensure they’re always updated whenever changes are made,… Some platforms or build-systems require generated files to be part of distributed packages though, and keeping record of what gets...","categories": ["Development"],
+        "tags": [],
+        "url": "/development/a-git-branching-model-for-releases-with-generated-files/",
+        "teaser": null
+      }]
